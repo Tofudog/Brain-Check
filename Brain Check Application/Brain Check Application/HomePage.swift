a933cd29681cc1c98112c8268249fdcfad1ac56c
Tofudog <leodefarias25@gmail.com>
2025-12-11 22:06:08 -0500

    Add title + button with states

--- Brain Check Application/Brain Check Application/HomePage.swift
+++ Brain Check Application/Brain Check Application/HomePage.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class HomePage: UIViewController {
     
+    @IBOutlet var checkBrainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Define background image for the home page
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "background-image")
         backgroundImage.contentMode = .scaleAspectFill
         view.insertSubview(backgroundImage, at: 0)
     }
     
+    @IBAction func clickButtonDown() {
+        // The state of the button when it is "touched down on"
+        let activeStateImage = UIImage(named: "check-button-active")
+        self.checkBrainButton.setImage(activeStateImage, for: .normal)
+    }
+    
+    @IBAction func clickButtonUp() {
+        // The state of the button when it is "touched up from down"
+        let inactiveStateImage = UIImage(named: "check-button-inactive")
+        self.checkBrainButton.setImage(inactiveStateImage, for: .normal)
+    }
 }
 
