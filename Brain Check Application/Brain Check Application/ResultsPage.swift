93c797ca8c0ec391230b49b4efff60c4c7fc4fea
Tofudog <leodefarias25@gmail.com>
2025-12-16 15:09:01 -0500

    Initialize Results Page

---
+++ Brain Check Application/Brain Check Application/ResultsPage.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsPage.swift
+//  Brain Check Application
+//
+//  Created by Leonardo Amato Regis de Farias on 12/16/25.
+//
+
+import Foundation
+import UIKit
+
+class ResultsPage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "ResultsPage"
+    }
+    
+}
